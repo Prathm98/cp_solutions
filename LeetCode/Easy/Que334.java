9885727c3ab0d5db24894f2f10eafebf2df0bd05
Prathmesh Wakodikar <46840479+Prathm98@users.noreply.github.com>
2025-05-07 23:45:10 +0530

    Leetcode Med Que334

---
+++ LeetCode/Easy/Que334.java
@@ -0,0 +1,18 @@
+public class Que334 {
+    // https://leetcode.com/problems/increasing-triplet-subsequence/description/
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= first) {
+                first = nums[i];
+            } else if (nums[i] <= second) {
+                second = nums[i];
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
